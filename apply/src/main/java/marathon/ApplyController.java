5a4ef8a62884ac2e4f88480446cfe129a9e4d37c
sasin4 <hyung82@gmail.com>
2021-09-15 22:01:35 +0900

    First commit

--- apply/src/main/java/marathon/ApplyController.java
+++ apply/src/main/java/marathon/ApplyController.java
@@ -10,7 +10,26 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
  @RestController
+ @RequestMapping("/apply")
  public class ApplyController {
-
+     
+	@Autowired
+    ApplyApplication applyApplication;
+	
+	//@ApiOperation(value = "결제 진행하기")
+	@PostMapping("/applied")
+	public boolean Applied(@RequestBody Apply application) {
+		
+		ApplyApplication saveApplication = applyApplication.save(application);
+		/*
+        // CB test 용 지연 코드.
+        try {
+            Thread.currentThread().sleep((long) (400 + Math.random() * 220));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }		
+		*/
+		return true;
+	}
 
  }
